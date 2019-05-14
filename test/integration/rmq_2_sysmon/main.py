53e36ad93b1ec3fb2603a24f3c6c01afbf8deddf
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-14 09:27:01 +0100

    Update test/integration/rmq_2_sysmon/main.py

--- test/integration/rmq_2_sysmon/main.py
+++ test/integration/rmq_2_sysmon/main.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python
 # Classification (U)
 
-###############################################################################
-#
-# Program:      main.py
-#
-# Class Dependencies:
-#               None
-#
-# Library Dependenices:
-#               rmq_2_sysmon    => v0.1.0 or higher
-#               rmq_cleanup     => v0.1.0 or higher
-#               lib.gen_libs    => v2.4.0 or higher
-#
-###############################################################################
-
 """Program:  main.py
 
     Description:  Integration testing of main in rmq_2_sysmon.py.
@@ -79,16 +65,12 @@ class UnitTest(unittest.TestCase):
         self.base_dir = "test/integration/rmq_2_sysmon"
         self.test_path = os.path.join(os.getcwd(), self.base_dir)
         self.config_path = os.path.join(self.test_path, "config")
-
         self.cfg = gen_libs.load_module("rabbitmq", self.config_path)
-
         log_path = os.path.join(self.test_path, self.cfg.log_dir)
         self.cfg.log_file = os.path.join(log_path, self.cfg.log_file)
-
         self.cfg.message_dir = os.path.join(self.test_path,
                                             self.cfg.message_dir)
         self.cfg.sysmon_dir = os.path.join(self.test_path, self.cfg.sysmon_dir)
-
         self.connect_true = "Connected to RabbitMQ node"
         self.argv_list = [os.path.join(self.base_dir, "main.py"), "-M", "-c",
                           "rabbitmq", "-d", "config"]
@@ -112,7 +94,6 @@ class UnitTest(unittest.TestCase):
         mock_consume.return_value = "RabbitMQ_Tag"
         mock_cfg.return_value = self.cfg
         mock_base.return_value = self.test_path
-
         rmq_2_sysmon.main(argv_list=self.argv_list)
 
         if self.connect_true in open(self.cfg.log_file).read():
@@ -135,7 +116,6 @@ class UnitTest(unittest.TestCase):
         """
 
         os.remove(self.cfg.log_file)
-
         rmq_cleanup.rmq_cleanup(self.cfg, self.cfg.queue_name, True)
 
 
