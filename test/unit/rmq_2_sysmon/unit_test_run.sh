ac7efb82c21f51faeac3be0937ce5599dc058b04
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-14 07:36:27 +0100

    Update test run files

--- test/unit/rmq_2_sysmon/unit_test_run.sh
+++ test/unit/rmq_2_sysmon/unit_test_run.sh
@@ -5,30 +5,12 @@
 #   is located at.
 
 echo ""
-echo "Unit test:  help_message"
+echo "Unit test:  rmq_2_sysmon.py"
 test/unit/rmq_2_sysmon/help_message.py
-
-echo ""
-echo "Unit test:  validate_create_settings"
 test/unit/rmq_2_sysmon/validate_create_settings.py
-
-echo ""
-echo "Unit test:  non_proc_msg"
 test/unit/rmq_2_sysmon/non_proc_msg.py
-
-echo ""
-echo "Unit test:  process_msg"
 test/unit/rmq_2_sysmon/process_msg.py
-
-echo ""
-echo "Unit test:  monitor_queue"
 test/unit/rmq_2_sysmon/monitor_queue.py
-
-echo ""
-echo "Unit test:  run_program"
 test/unit/rmq_2_sysmon/run_program.py
-
-echo ""
-echo "Unit test:  main"
 test/unit/rmq_2_sysmon/main.py
 
