d5f0a583cdc81d1be9785fb302ac12f9d36e8985
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-09-11 14:48:52 +0100

    Documentation update for unit test files

--- test/unit/rmq_2_sysmon/process_msg.py
+++ test/unit/rmq_2_sysmon/process_msg.py
@@ -37,10 +37,6 @@ class UnitTest(unittest.TestCase):
 
     Description:  Class which is a representation of a unit testing.
 
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
-
     Methods:
         setUp -> Initialize testing environment.
         test_key_not_in_dict -> Test if key is not in dictionary.
@@ -67,10 +63,6 @@ class UnitTest(unittest.TestCase):
             """Class:  CfgTest
 
             Description:  Class which is a representation of a cfg module.
-
-            Super-Class:  object
-
-            Sub-Classes:
 
             Methods:
                 __init__ -> Initialize configuration environment.
--- test/unit/rmq_2_sysmon/validate_create_settings.py
+++ test/unit/rmq_2_sysmon/validate_create_settings.py
@@ -38,10 +38,6 @@ class UnitTest(unittest.TestCase):
 
     Description:  Class which is a representation of a unit testing.
 
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
-
     Methods:
         setUp -> Initialize testing environment.
         test_multiple_false2 -> Test if multiple checks return False.
@@ -70,10 +66,6 @@ class UnitTest(unittest.TestCase):
             """Class:  CfgTest
 
             Description:  Class which is a representation of a cfg module.
-
-            Super-Class:  object
-
-            Sub-Classes:
 
             Methods:
                 __init__ -> Initialize configuration environment.
