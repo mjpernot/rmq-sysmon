47711535f9be503eda19f6fc64ad3ef4ac529576
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-06-22 15:40:39 +0100

    Update test/blackbox/rmq_2_sysmon/blackbox_test.sh v2.0.0

--- test/blackbox/rmq_2_sysmon/blackbox_test.sh
+++ test/blackbox/rmq_2_sysmon/blackbox_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 # Blackbox testing program for the rmq_2_sysmon.py program.
 
 BASE_PATH=$PWD
@@ -27,7 +27,7 @@ test/blackbox/rmq_2_sysmon/blackbox_test.py
 ./daemon_rmq_2_sysmon.py -c rabbitmq -d ${BASE_PATH}/test/blackbox/rmq_2_sysmon/config -M -a stop
 test/blackbox/rmq_2_sysmon/cleanup.py
 
-echo "Scenario 4:  rmq_2_sysmon blackbox testing...Pass non-sysmon report"
+echo "Scenario 4:  rmq_2_sysmon blackbox testing...Pass non-type report"
 test/blackbox/rmq_2_sysmon/rmq_cleanup.py
 ./daemon_rmq_2_sysmon.py -c rabbitmq -d ${BASE_PATH}/test/blackbox/rmq_2_sysmon/config -M -a start
 test/blackbox/rmq_2_sysmon/blackbox_test3.py
