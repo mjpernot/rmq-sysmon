f6e61dcaf619a3912673cb31681bf9602515cb30
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-06-30 14:38:10 +0100

    Update test/integration/rmq_2_sysmon/main.py v2.0.0

--- test/integration/rmq_2_sysmon/main.py
+++ test/integration/rmq_2_sysmon/main.py
@@ -91,13 +91,7 @@ class UnitTest(unittest.TestCase):
         mock_base.return_value = self.test_path
         rmq_2_sysmon.main(argv_list=self.argv_list)
 
-        if self.connect_true in open(self.cfg.log_file).read():
-            status = True
-
-        else:
-            status = False
-
-        self.assertTrue(status)
+        self.assertTrue(self.connect_true in open(self.cfg.log_file).read())
 
     def tearDown(self):
 
