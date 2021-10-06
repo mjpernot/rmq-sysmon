b0f43430c4be511e8c2a0e7726f260f324663800
Mark Pernot <mark.pernot@gmail.com>
2021-10-06 12:23:38 +0100

    Update rmq_2_sysmon.py v2.1.0

--- rmq_2_sysmon.py
+++ rmq_2_sysmon.py
@@ -392,7 +392,7 @@ def monitor_queue(cfg, log):
 
     for queue in cfg.queue_list:
         rmq = rabbitmq_class.RabbitMQCon(
-            cfg.user, cfg.passwd, cfg.host, cfg.port,
+            cfg.user, cfg.japd, cfg.host, cfg.port,
             exchange_name=cfg.exchange_name, exchange_type=cfg.exchange_type,
             queue_name=queue["queue"], routing_key=queue["routing_key"],
             x_durable=cfg.x_durable, q_durable=cfg.q_durable,
@@ -416,7 +416,7 @@ def monitor_queue(cfg, log):
 
     # Connect to first queue as only one connection required.
     rmq = rabbitmq_class.RabbitMQCon(
-        cfg.user, cfg.passwd, cfg.host, cfg.port,
+        cfg.user, cfg.japd, cfg.host, cfg.port,
         exchange_name=cfg.exchange_name, exchange_type=cfg.exchange_type,
         queue_name=cfg.queue_list[0]["queue"],
         routing_key=cfg.queue_list[0]["routing_key"],
